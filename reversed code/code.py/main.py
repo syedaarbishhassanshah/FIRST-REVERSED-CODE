3569fce9690f41281bad6dd75da11b6af3c6ad46
SYEDA ARBISH <jugnoo708@gmail.com>
2024-07-09 19:10:17 +0500

    Add files via upload

---
+++ reversed code/code.py/main.py
@@ -0,0 +1,9 @@
+
+# given string
+Quote= ("Learning  is never done without error, and defeat ")
+words =Quote.split()
+# Reversed the list of words
+reversed_words = words[::-1]
+# join the reversed list of words back into Quote
+reversed_Quote = ' '.join(reversed_words)
+print (reversed_Quote )
